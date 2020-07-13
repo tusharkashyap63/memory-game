b9e66966b738d5cfa073ac74af87bc93c92ff693
Tushar Kashyap <kashyap.tushar15@gmail.com>
2020-07-13 15:40:08 +0530

    Finished

---
+++ memoryMatch.js
@@ -0,0 +1,106 @@
+//global variables go here:
+var clickedArray = [];
+var interval;
+var started = false;
+var time = 0;
+var ready = true;
+var numCompleted = 0;
+//execute functions here:
+setUp();
+
+//function definitions go here:
+function randomAnswers() {
+  var answers = [1, 1, 2, 2, 3, 3, 4, 4, 5];
+  answers.sort(function (item) {
+    return 0.5 - Math.random();
+  });
+  return answers;
+}
+
+function setUp() {
+  let grid = document.querySelectorAll('td');
+  let answers = randomAnswers();
+
+  for (let i = 0; i < grid.length; i++) {
+    let cell = grid[i];
+    cell.completed = false;
+    cell.clicked = false;
+    cell.value = answers[i];
+    cell.addEventListener('mouseenter', function () {
+      if (this.completed == false && this.clicked == false) this.style.background = 'orange';
+    });
+    cell.addEventListener('mouseleave', function () {
+      if (this.completed == false && this.clicked == false) this.style.background = 'blue';
+    });
+    cell.addEventListener('click', function () {
+      if (ready == false) return;
+      if (this.completed == false && this.clicked == false) {
+        startTimer();
+        clickedArray.push(this);
+        reveal(this);
+        if (clickedArray.length == 2) {
+          if (clickedArray[0].value == clickedArray[1].value) {
+            complete(clickedArray[0]);
+            complete(clickedArray[1]);
+
+            clickedArray = [];
+
+            if (numCompleted == 8) {
+              alert('You won in ' + time + ' seconds!');
+              clearInterval(interval);
+            }
+          } else {
+            ready = false;
+            document.getElementById('gridTable').style.border = '5px solid red';
+
+            setTimeout(function () {
+              hide(clickedArray[0]);
+              hide(clickedArray[1]);
+
+              clickedArray = [];
+
+              ready = true;
+              document.getElementById('gridTable').style.border = '5px solid black';
+            }, 500);
+          }
+        }
+      }
+    });
+  }
+  document.addEventListener('keydown', function (event) {
+    if (event.key > 0 && event.key < 10) {
+      grid[event.key - 1].click();
+    }
+  });
+  document.getElementById('restart').addEventListener('click', function () {
+    location.reload();
+  });
+}
+
+function reveal(cell) {
+  cell.style.backgroundColor = 'red';
+  cell.innerHTML = cell.value;
+  cell.clicked = true;
+}
+
+function startTimer() {
+  if (started == false) {
+    interval = setInterval(() => {
+      time++;
+      document.getElementById('timer').innerHTML = 'Time Elapsed: ' + time;
+    }, 1000);
+    started = true;
+  }
+}
+
+function hide(cell) {
+  cell.style.backgroundColor = 'blue';
+  cell.innerHTML = '';
+  cell.clicked = false;
+}
+
+function complete(cell) {
+  numCompleted++;
+  cell.completed = true;
+  cell.style.backgroundColor = 'purple';
+}
